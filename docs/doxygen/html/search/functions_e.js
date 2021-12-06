0eb6f624df529347fe087c5826d0c176794b185e
Zhengshou Lai <lai.zhengshou@outlook.com>
2021-12-06 11:08:35 +0800

    udpate doxygen

--- docs/doxygen/html/search/functions_e.js
+++ docs/doxygen/html/search/functions_e.js
@@ -4,5 +4,6 @@ var searchData=
   ['quaternionconjugate_1',['QuaternionConjugate',['../namespacenetdem.html#aeb30855c629550e3974c2ecf86514abe',1,'netdem']]],
   ['quaternionmultiply_2',['QuaternionMultiply',['../namespacenetdem.html#aadf86ecfdc9972a58513e02eef5e7c86',1,'netdem']]],
   ['quaternionnormalize_3',['QuaternionNormalize',['../namespacenetdem.html#a5781b8f7c6013952a85764e0d2642398',1,'netdem']]],
-  ['quaterniontorodrigues_4',['QuaternionToRodrigues',['../namespacenetdem.html#acf11d140dac452d691da9bbdc0f2c7a5',1,'netdem']]]
+  ['quaterniontomatrix_4',['QuaternionToMatrix',['../namespacenetdem.html#a8e99458fe0c51c427e12d90cadb10b35',1,'netdem']]],
+  ['quaterniontorodrigues_5',['QuaternionToRodrigues',['../namespacenetdem.html#a14cf738f32e151c33964a69adb7fa855',1,'netdem']]]
 ];
